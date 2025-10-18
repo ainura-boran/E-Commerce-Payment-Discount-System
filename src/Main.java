7562123ea9cbb9bfae0a636815ef8e84c4f39fd3
Ainur Boranova <241970@astanait.edu.kz>
2025-10-18 23:15:29 +0500

    final version

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,43 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Customer customer = new Customer(
-                "Ainur", "boranova2007@mail.ru", "Aktau, Kazakhstan", 120);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("--- Creating customer's profile ---");
+        System.out.print("Enter your full name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your email: ");
+        String email = sc.nextLine();
+
+        System.out.print("Enter your address: ");
+        String address = sc.nextLine();
+
+        Customer customer = new Customer(name, email, address, 0);
+        System.out.println("Profile is created");
 
         Cart cart = new Cart();
-        cart.add(new BigDecimal("200"));
-        cart.add(new BigDecimal("150"));
-        cart.add(new BigDecimal("50"));
+        System.out.println("--- Adding items to cart ---");
+        while (true) {
+            System.out.print("Enter item's price (press Enter to finish): ");
+            String input = sc.nextLine();
 
+            if (input.equals("")) break;
+            try {
+                BigDecimal price = new BigDecimal(input);
+                cart.add(price);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input, try again.");
+            }
+        }
+
+        System.out.println("Total price: " + cart.total() + " ₸");
         System.out.println("Select the type of payment:");
         System.out.println("1) Visa");
         System.out.println("2) MasterCard");
         System.out.println("3) PayPal");
         System.out.print("Your choice: ");
 
-        Scanner sc = new Scanner(System.in);
         int choice = sc.nextInt();
 
         PaymentType type = switch (choice) {
